a5f5b8ff521dd153e996b906121f89aa63a432d1
1Gbattle <garybattle321@gmail.com>
2023-11-22 20:38:25 -0600

    started implementation of the login and sign up pages

---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,45 @@
+<!-- create login page using tailwind css -->
+
+<script>
+	import { onMount } from 'svelte';
+	import { page } from '$app/stores';
+	import { writable } from 'svelte/store';
+
+	export let email = '';
+	export let password = '';
+	export let error = '';
+
+	const pageStore = writable(page);
+</script>
+
+<div class="flex flex-col items-center justify-center h-screen">
+	<div class="flex flex-col items-center justify-center w-1/2">
+		<h1 class="text-4xl font-medium">Login</h1>
+		<form class="flex flex-col items-center justify-center w-full">
+			<div class="flex flex-col items-center justify-center w-full">
+				<label for="email" class="text-xl font-medium">Email</label>
+				<input
+					type="email"
+					name="email"
+					id="email"
+					class="p-2 w-full rounded-lg"
+					bind:value={email}
+				/>
+			</div>
+			<div class="flex flex-col items-center justify-center w-full">
+				<label for="password" class="text-xl font-medium">Password</label>
+				<input
+					type="password"
+					name="password"
+					id="password"
+					class="p-2 w-full rounded-lg"
+					bind:value={password}
+				/>
+			</div>
+			{#if error}
+				<p class="text-red-600">{error}</p>
+			{/if}
+			<button type="submit" class="p-2 w-full bg-green-400 rounded-lg">Login</button>
+		</form>
+	</div>
+</div>
--- src/routes/todos/[id]/+page.svelte
+++ src/routes/todos/[id]/+page.svelte
@@ -1,11 +1,11 @@
 <script></script>
 
 <div class="h-full">
-	<div class="p-2 bg-cyan-400 h-1/5">
+	<div class="p-2 bg-cyan-400 h-2/6">
 		<h1 class="font-medium text-3xl">Todo Name</h1>
 	</div>
 
-	<div class="flex flex-col items-start justify-around h-4/6">
+	<div class="flex flex-col items-start justify-around h-3/6">
 		<div class="p-2">
 			<h2 class="font-medium text-2xl">Due Date:</h2>
 			<p class="text-lg p-2">fake date</p>
@@ -14,15 +14,19 @@
 			<h2 class="font-medium text-2xl">Urgency:</h2>
 			<p class="text-lg p-2">Urgent</p>
 		</div>
-		<div class="p-2">
+		<div class="p-2 overflow-auto">
 			<h2 class="font-medium text-2xl">Description:</h2>
 			<p class="text-lg p-2">
-				Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis porro quibusdam minima.
+				Fugit odio reiciendis obcaecati sit quae natus explicabo perferendis, at debitis quo
+				voluptatem accusantium impedit consectetur placeat quod. Lorem ipsum dolor sit, amet
+				consectetur adipisicing elit. Aut, officiis necessitatibus molestiae ipsa nam eum earum hic
+				minus quod ipsam, praesentium fugiat quo? Non animi, maiores eius nihil quia quod.
 			</p>
 		</div>
 	</div>
 
-	<div class="p-4 w-full flex flex-row justify-between gap-8">
+	<div class="p-4 w-full mt-16 flex flex-row justify-between gap-8">
 		<button class="p-2 w-1/2 bg-yellow-400 rounded-lg">Edit</button>
 		<button class="p-2 w-1/2 bg-red-600 rounded-lg">Delete</button>
 	</div>
