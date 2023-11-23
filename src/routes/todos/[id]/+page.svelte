a535fc3c19b80fc5dd2758e2dfe21cc6d1db0aa1
1Gbattle <garybattle321@gmail.com>
2023-11-22 20:22:00 -0600

    completed styling for todo page

---
+++ src/routes/todos/[id]/+page.svelte
@@ -0,0 +1,29 @@
+<script></script>
+
+<div class="h-full">
+	<div class="p-2 bg-cyan-400 h-1/5">
+		<h1 class="font-medium text-3xl">Todo Name</h1>
+	</div>
+
+	<div class="flex flex-col items-start justify-around h-4/6">
+		<div class="p-2">
+			<h2 class="font-medium text-2xl">Due Date:</h2>
+			<p class="text-lg p-2">fake date</p>
+		</div>
+		<div class="p-2">
+			<h2 class="font-medium text-2xl">Urgency:</h2>
+			<p class="text-lg p-2">Urgent</p>
+		</div>
+		<div class="p-2">
+			<h2 class="font-medium text-2xl">Description:</h2>
+			<p class="text-lg p-2">
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.
+			</p>
+		</div>
+	</div>
+
+	<div class="p-4 w-full flex flex-row justify-between gap-8">
+		<button class="p-2 w-1/2 bg-yellow-400 rounded-lg">Edit</button>
+		<button class="p-2 w-1/2 bg-red-600 rounded-lg">Delete</button>
+	</div>
+</div>
